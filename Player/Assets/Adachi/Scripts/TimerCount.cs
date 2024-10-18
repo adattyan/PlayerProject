4f315d172ac26f7f7ab8bd148fa1deeba2c29217
YDA-R04-MSI10\nwuser <admano423@gmail.com>
2024-10-18 09:06:37 +0900

    9月17日分

--- Player/Assets/Adachi/Scripts/TimerCount.cs
+++ Player/Assets/Adachi/Scripts/TimerCount.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerCount : MonoBehaviour
 {
+    [SerializeField] float time = 60;   //時間
+    [SerializeField] GameObject Timer;  //ゲームオブジェクトとしてのtimer
+    Text timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,17 @@ public class TimerCount : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Count();
+    }
+
+    /// <summary>
+    /// 時間を計測
+    /// </summary>
+    void Count()
+    {
+        time -= Time.deltaTime;
+        timer.text = "残り秒";
     }
 }
